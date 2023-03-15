901929fd0a78dc1ef41a68cad1060dd753448421
RyanKaleliGabriel <ryang9241@gmail.com>
2023-03-15 17:21:53 +0300

    Level 3 Authentication Hashing

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-
+const md5 = require("md5")
 const app = express();
 
-console.log(process.env.API_KEY);
+console.log(md5("user"));
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -24,7 +23,6 @@ const userSchema = new mongoose.Schema({
 });
 
 //LEVEL 2 Authentication . 1 add a key to encrypt the database(SECRET IN THE .ENV FILE). 2. Use the plugin
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields:["password"]});
 
 
 const User = new mongoose.model("User", userSchema);
@@ -49,7 +47,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
         const newUser = new User({
             email:req.body.username,
-            password:req.body.password
+            password:md5(req.body.password)
         });
 
         try{
@@ -61,7 +59,7 @@ app.post("/register", function(req, res){
 });
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}).then((foundUser) =>{
         if(foundUser){
