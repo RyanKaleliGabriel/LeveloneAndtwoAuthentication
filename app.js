a736f335879a308833b05c96325de872f6d6875a
RyanKaleliGabriel <ryang9241@gmail.com>
2023-03-15 20:19:01 +0300

    Level 3 authentication Bcrypt Salting and Hashing

--- app.js
+++ app.js
@@ -4,23 +4,24 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5")
+const bcrypt = require("bcrypt")
+const saltRounds = 10;
+
 const app = express();
 
-console.log(md5("user"));
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
-
 mongoose.set("strictQuery", true);
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true});
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser:true});
 
 // For Level 2 (Database authentication) authentication turn your schema to a proper mongoose schema object (new)
 const userSchema = new mongoose.Schema({
     email: String,
     password:String
 });
+
 
 //LEVEL 2 Authentication . 1 add a key to encrypt the database(SECRET IN THE .ENV FILE). 2. Use the plugin
 
@@ -45,27 +46,34 @@ app.get("/register", function(req, res){
 // 1. LEVEL ONE AUTHENTICATION ///////
 
 app.post("/register", function(req, res){
+    bcrypt.hash(req.body.password, saltRounds,function(err, hash){
         const newUser = new User({
             email:req.body.username,
-            password:md5(req.body.password)
+            password:hash
         });
-
         try{
             newUser.save();
         }catch(error){
             console.error(error);
         }
         res.render("secrets");
+    });
+
+
+
 });
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email: username}).then((foundUser) =>{
         if(foundUser){
-            if(foundUser.password === password){
-                res.render("secrets");
-            }
+            bcrypt.compare(password, foundUser.password).then(function(result) {
+                if(result){
+                    res.render("secrets")
+                }
+            });
+
         }
     }).catch((error) =>{
         console.error(error);
